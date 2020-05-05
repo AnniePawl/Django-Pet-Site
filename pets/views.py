36010fef011d5ef7d409148090094890ee24b7f9
Anna Pawl <annampawl@gmail.com>
2020-05-05 19:16:39 -0400

    added pet home url, created home view and template

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Pet
+from .models import Appointment
 
-# Create your views here.
+
+# HOME VIEW
+def home(request):
+    context = {
+
+    }
+    return render(request, 'pets/home.html', context)
+
+
+# PETS LIST PAGE VIEW
+  # Lists all user's pets (bullet points)
+  # Pets names must link to detail pages uses {% url%} template tag
+  # must include form to create new pet or link to seperate creation page
+  # should not be vieable by non-logged-in users
+
+
+# PETS DETAIL VIEW
+  # Must show all relevant details about pet
+  # must list all appointments associated with pet
+  # Should not be viewable by non-logged-in users
+
+
+# CALENDAR PAGE
+  # Must show all upcoming appointments w/ all pets, earliest first
+  # Should not show appointments that have already occured
+  # Must include form to create new appointment of link to seperate appointment creation page
+  # should not be viewable by non logged in users
+
+# LOGIN PAGE
+
+# SIGNUP PAGE
